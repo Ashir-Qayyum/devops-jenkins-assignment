bc12b5ffa55bd6b7230b52f1768ed4c9bcdf0068
ashir-qayyum <muhammadashir2004@gmail.com>
2026-03-27 09:56:52 +0000

    Part 07 - TESTING THE PIPELINE ATTEMPT 08 build 07

--- python-basic-app.py
+++ python-basic-app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Ashir app for Assignment 06. TRIGGERED VIA WEBHOOK using CLOUDFLARED!!!"
+    return "Welcome to Ashir app for Assignment 06. TRIGGERED VIA WEBHOOK using CLOUDFLARED!!! 7th BUILD" 
 
 
 
