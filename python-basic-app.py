9b485ff02f2ff927c136580ce1061a44f0e6cbf1
ashir-qayyum <muhammadashir2004@gmail.com>
2026-03-27 08:23:32 +0000

    Part 07 - TESTING THE PIPELINE ATTEMPT 07

--- python-basic-app.py
+++ python-basic-app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Ashir app for Assignment 06. Modified to test Pipeline Manually! Now Modified to test Automatic CI/CD Trigger via Webhook!! Try Again: Now with Cloudflare! AFTER THE REBOOT"
+    return "Welcome to Ashir app for Assignment 06. TRIGGERED VIA WEBHOOK using CLOUDFLARED!!!"
 
 
 
