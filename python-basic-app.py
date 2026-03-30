944ed2bbd7fcd250774dcc2e36140e3aabb6e35e
ashir-qayyum <muhammadashir2004@gmail.com>
2026-03-30 13:43:46 +0000

    Testing - 20th build

--- python-basic-app.py
+++ python-basic-app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Ashir app for Assignment 06. TRIGGERED VIA WEBHOOK using CLOUDFLARED!!! 19th BUILD -Again Check" 
+    return "Welcome to Ashir app for Assignment 06. TRIGGERED VIA WEBHOOK using CLOUDFLARED!!! 20th BUILD -Again Check" 
 
 
 
